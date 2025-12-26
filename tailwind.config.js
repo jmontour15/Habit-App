fbd77c6c1a094ccaea823a7899fcd5f4c79f7195
jmontour15 <jmontour@uvm.edu>
2025-12-26 11:39:50 -0500

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#2563eb",
+      },
+    },
+  },
+  plugins: [],
+};
